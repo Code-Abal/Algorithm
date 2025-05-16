bdd2b3c5edd728e325ea051b532f20a3b0b36fe6
Code.D1 <119289144+Code-Abal@users.noreply.github.com>
2025-05-16 12:40:32 +0900

    [D3] Title: 공평한 분배 2, Time: 10 ms, Memory: 5,840 KB -BaekjoonHub

--- SWEA/D3/20728. 공평한 분배 2/공평한 분배 2.cpp
+++ SWEA/D3/20728. 공평한 분배 2/공평한 분배 2.cpp
@@ -2,13 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
-#include <climits>
 using namespace std;
-
-int compare(const int& a, const int& b)
-{
-	return a < b;
-}
 
 int main()
 {
@@ -17,8 +11,6 @@ int main()
 
 	for (int tc = 1; tc <= T; tc++)
 	{
-		//첫 번째 예제: 주머니 2개를 고르는 방법은 {1, 2}, {1, 3}, {2, 3}이 있다. 
-		//각각의 (최대) – (최소) 값은 1, 2, 1이다. 이 중 최솟값은 1이다.
 		int N, K;
 		cin >> N >> K;
 
@@ -29,16 +21,19 @@ int main()
 			cin >> pocket[i];
 		}
 
-		sort(pocket.begin(), pocket.end(), compare);
-
-		int mincandy = INT_MAX;
-		for (int i = 0; i <= N - K ; i++)
-
+		sort(pocket.begin(), pocket.end(), [](int& A, int& B)
 		{
-			mincandy = min(mincandy, pocket[i + K - 1] - pocket[i]);
+			return A < B;
+		});
+
+		int min_candy = 1000000001;
+		for (int i = 0; i <= N - K; i++)
+		{
+			min_candy = min(min_candy, pocket[i + K - 1] - pocket[i]);
 		}
 
-		cout << "#" << tc << " " << mincandy << "\n";
+		cout << "#" << tc << " " << min_candy << "\n";
+
 	}
 	return 0;
 }  
