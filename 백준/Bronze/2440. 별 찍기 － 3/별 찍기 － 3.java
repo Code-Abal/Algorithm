a568db5847a7a1d38e5700bc4df9323a60b005cc
Code.D1 <119289144+Code-Abal@users.noreply.github.com>
2025-07-28 22:34:38 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 172 ms, Memory: 14964 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for(int i = N; i >= 1; --i)
+		{
+			for(int j = 1; j <= i; j++)
+			{
+				System.out.print("*");
+			}
+			
+			for(int space = 1; space <= N - i; ++space)
+			{
+				//System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
