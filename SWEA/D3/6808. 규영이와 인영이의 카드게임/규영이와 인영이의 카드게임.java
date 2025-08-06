2bc65cfa349eddb3c075827d6eeb0a404f54e56d
Code.D1 <119289144+Code-Abal@users.noreply.github.com>
2025-08-06 22:45:55 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 2,286 ms, Memory: 27,520 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -1,9 +1,7 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
-
 
 public class Solution {
 	static int win = 0;
@@ -70,16 +68,9 @@ public class Solution {
 		for(int i = 0; i < 9; i++)
 		{
 			int score = kyu[i] + inyung[i];
-			if(kyu[i] > inyung[i])
-			{
-				scoreK += score;
-			}
-			else if(kyu[i] < inyung[i])
-			{
-				scoreI += score;
-			}
+			if(kyu[i] > inyung[i]) scoreK += score;
+			else if(kyu[i] < inyung[i]) scoreI += score;
 		}
-		
 		if(scoreK > scoreI) win++;
 		else lose++;
 	}
