cefd4472ae6e33b8df5cba6ad40d24a728b6d441
Code.D1 <119289144+Code-Abal@users.noreply.github.com>
2025-10-29 16:50:19 +0900

    [Gold IV] Title: 암벽 등반, Time: 156 ms, Memory: 16816 KB -BaekjoonHub

--- 백준/Gold/2412. 암벽 등반/암벽 등반.java
+++ 백준/Gold/2412. 암벽 등반/암벽 등반.java
@@ -1,21 +1,12 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 	static int N, T, cnt = -1;
 	static List<Integer>[] xList;
 	
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-        
-		N = Integer.parseInt(st.nextToken()); // 홈의 수
-		T = Integer.parseInt(st.nextToken()); // 암벽의 정상
+		N = read(); // 홈의 수
+		T = read(); // 암벽의 정상
 		
 		// 리스트 초기화
 		xList = new ArrayList[T+1];
@@ -24,33 +15,27 @@ public class Main {
 		}
 		
 		// 입력
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
+		for(int i=0; i < N; i++) {
+			int x = read();
+			int y = read();
 			
 			xList[y].add(x);
 		}
-		bfs(0, 0);
+		bfs();
 		System.out.println(cnt);
 	}
 	
-	static void bfs(int x, int y) {
+	static void bfs() {
 		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] {y, x, 0});
+		q.add(new int[] {0, 0, 0});
 		
 		while(!q.isEmpty()) {
 			int[] cur = q.poll();
 			
-			if (cur[1] == T) {
-				cnt = cur[2];
-				return;
-			}
+			if (cur[1] == T) {cnt = cur[2]; return;}
 			
 			for(int ny=cur[1]-2; ny<=cur[1]+2; ny++) {
-				if (ny < 0 || ny > T) {
-					continue;
-				}
+				if (ny < 0 || ny > T) continue;
 				
 				for(int i = 0; i<xList[ny].size(); i++) {
 					int nx = xList[ny].get(i);
@@ -64,4 +49,18 @@ public class Main {
 			}
 		}
 	}
+	
+	private static int read() throws Exception {
+	    int c;
+	    while ((c = System.in.read()) <= 32) {
+	        if (c == -1) return -1; // EOF
+	    }
+
+	    int n = c & 15;
+	    while ((c = System.in.read()) > 32) {
+	        n = (n << 3) + (n << 1) + (c & 15);
+	    }
+	    return n;
+	}
+
 }
