e4c60e911e263a5ddd53e59d516469eb0555de70
Code.D1 <119289144+Code-Abal@users.noreply.github.com>
2025-10-29 17:15:09 +0900

    [Gold IV] Title: 암벽 등반, Time: 16 ms, Memory: 7108 KB -BaekjoonHub

--- 백준/Gold/2412. 암벽 등반/암벽 등반.cc
+++ 백준/Gold/2412. 암벽 등반/암벽 등반.cc
@@ -4,45 +4,41 @@
 using namespace std;
 
 int N, T, cnt = -1;
-vector<int> xList[200001]; // T 최대값에 따라 충분히 크게 선언 (문제에 맞게 조정)
+vector<int> xList[200001]; 
 
 void bfs()
 {
 	queue<vector<int>> q;
-	q.push({ 0, 0, 0 }); // {x, y, cnt}
+	q.push({ 0, 0, 0 }); 
 
 	while (!q.empty()) {
 		vector<int> info = q.front();
-		int x = info[0];
-		int y = info[1];
-		int step = info[2];
-
 		q.pop();
 
-		if (y == T) {
-			cnt = step;
+		if (info[1] == T) {
+			cnt = info[2];
 			return;
 		}
 
-		for (int ny = y - 2; ny <= y + 2; ny++) {
+		for (int ny = info[1] - 2; ny <= info[1] + 2; ny++) {
 			if (ny < 0 || ny > T) continue;
 
-			for (int i = 0; i < (int)xList[ny].size(); i++) {
+			for (int i = 0; i < xList[ny].size(); i++) {
 				int nx = xList[ny][i];
-				if (abs(x - nx) <= 2) {
-					q.push({ nx, ny, step + 1 });
-					xList[ny].erase(xList[ny].begin() + i); // 방문처리
+				if (abs(info[0] - nx) <= 2) {
+					q.push({ nx, ny, info[2] + 1 });
+					xList[ny].erase(xList[ny].begin() + i);
 					i--;
 				}
 			}
 		}
-	}
+    }
 }
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
-
+    cout.tie(nullptr);
 	cin >> N >> T;
 	for (int i = 0; i < N; i++) {
 		int x, y;
