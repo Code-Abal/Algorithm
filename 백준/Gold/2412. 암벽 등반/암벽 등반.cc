fa5995358ec108c46dbaab5d23d11f0af9277e05
Code.D1 <119289144+Code-Abal@users.noreply.github.com>
2025-10-30 09:44:31 +0900

    [Gold IV] Title: 암벽 등반, Time: 16 ms, Memory: 4252 KB -BaekjoonHub

--- 백준/Gold/2412. 암벽 등반/암벽 등반.cc
+++ 백준/Gold/2412. 암벽 등반/암벽 등반.cc
@@ -1,47 +1,56 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
 
+struct Climbing
+{
+	int x, y, cnt;
+	Climbing(int _x, int _y, int _cnt) { x = _x; y = _y; cnt = _cnt; }
+};
+
 int N, T, cnt = -1;
-vector<int> xList[200001]; 
+vector<Climbing> xList[50001];
 
 void bfs()
 {
-	queue<vector<int>> q;
-	q.push({ 0, 0, 0 }); 
+	queue<Climbing> q;
+	q.push(Climbing(0,0,0));
 
 	while (!q.empty()) {
-		vector<int> info = q.front();
+		Climbing info = q.front();
 		q.pop();
 
-		if (info[1] == T) {
-			cnt = info[2];
+		if (info.y == T) {
+			cnt = info.cnt;
 			return;
 		}
 
-		for (int ny = info[1] - 2; ny <= info[1] + 2; ny++) {
+		for (int ny = info.y - 2; ny <= info.y + 2; ny++) {
 			if (ny < 0 || ny > T) continue;
 
 			for (int i = 0; i < xList[ny].size(); i++) {
-				int nx = xList[ny][i];
-				if (abs(info[0] - nx) <= 2) {
-					q.push({ nx, ny, info[2] + 1 });
+				int nx = xList[ny][i].x;
+
+				if (abs(info.x - nx) <= 2) {
+					q.push(Climbing( nx, ny, info.cnt + 1 ));
 					xList[ny].erase(xList[ny].begin() + i);
 					i--;
 				}
 			}
 		}
-    }
+	}
 }
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
-    cout.tie(nullptr);
+	cout.tie(nullptr);
 	cin >> N >> T;
 	for (int i = 0; i < N; i++) {
 		int x, y;
 		cin >> x >> y;
-		xList[y].push_back(x);
+		xList[y].push_back(Climbing(x, y, 0));
 	}
 
 	bfs();
