a4409a9e7f7c338442668f48d16fce98f163259a
Code.D1 <119289144+Code-Abal@users.noreply.github.com>
2025-12-01 13:35:15 +0900

    [Bronze I] Title: ISBN, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/14626. ISBN/ISBN.cc
+++ 백준/Bronze/14626. ISBN/ISBN.cc
@@ -8,26 +8,15 @@ int main()
 	for (int i = 0; i < 13; i++)
 	{
 		char isbn; cin >> isbn;
-
 		if (isbn != '*')
 		{
 			int w = i % 2 == 0 ? 1 : 3;
 			sum += (isbn - '0') * w;
 		}
-		else
-		{
-			unknown = i % 2 == 0 ? 1 : 3;
-		}
+		else unknown = i % 2 == 0 ? 1 : 3;
 	}
 	
-	for (int i = 0; i <= 9; i++)
-	{
-		if ((sum + unknown * i) % 10 == 0)
-		{
-			cout << i;
-			break;
-		}
-	}
+	for (int i = 0; i <= 9; i++) { if ((sum + unknown * i) % 10 == 0) { cout << i; break; } }
 
 	return 0;
 }
