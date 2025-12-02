ade96cc410076fc73fcb9685fdd1348ede4179ef
Code.D1 <119289144+Code-Abal@users.noreply.github.com>
2025-12-02 21:53:21 +0900

    [Silver III] Title: 걸그룹 마스터 준석이, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/16165. 걸그룹 마스터 준석이/걸그룹 마스터 준석이.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <map>
+using namespace std;
+
+int main()
+{
+	int N, M; cin >> N >> M;
+	map<string,set<string>> idol;
+	for (int i = 0; i < N; i++)
+	{
+		string group_name; cin >> group_name;
+		int head_count; cin >> head_count;
+
+		for (int cnt = 0; cnt < head_count; cnt++)
+		{
+			string name; cin >> name;
+			idol[group_name].insert(name);
+		}
+	}
+
+	for (int i = 0; i < M; i++)
+	{
+		string name; cin >> name;
+		int cmd; cin >> cmd;
+		if (cmd == 1)
+		{
+			for (auto m : idol)
+			{
+				if (m.second.find(name) != m.second.end())
+				{
+					cout << m.first << "\n";
+					break;
+				}
+			}
+		}
+		else
+		{
+			for (auto m : idol)
+			{
+				if (m.first == name)
+				{
+					for (auto s : m.second)
+					{
+						cout << s << "\n";
+					}
+					break;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
