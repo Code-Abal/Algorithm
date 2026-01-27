5d86308d08504afabbfa1faed08ceac807a6e90b
Code.D1 <119289144+Code-Abal@users.noreply.github.com>
2026-01-27 23:08:23 +0900

    [Silver V] Title: 최대 상승, Time: 100 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/25644. 최대 상승/최대 상승.cc
+++ 백준/Silver/25644. 최대 상승/최대 상승.cc
@@ -8,31 +8,13 @@ int N, profit = 0;
 int main()
 {
 	cin >> N;
-	for (int i = 0; i < N; i++) cin >> price[i];
-
-	int cur_price = price[N - 1];
-	int min_price = price[N - 1];
+	int min_price = 1e9;
 	int max_profit = 0;
-	for (int i = N - 1; i >= 0; i--)
+	for (int i = 0; i < N; i++)
 	{
-		if (cur_price > price[i])
-		{
-			min_price = min(min_price, price[i]);
-			if(i == 0)
-			{
-				profit = cur_price - min_price;
-				max_profit = max(max_profit, profit);
-				cur_price = price[i];
-				min_price = price[i];
-			}
-		}
-		else
-		{
-			profit = cur_price - min_price;
-			max_profit = max(max_profit, profit);
-			cur_price = price[i];
-			min_price = price[i];
-		}
+		int stock; cin >> stock;
+		max_profit = max(max_profit, stock - min_price);
+		min_price = min(min_price, stock);
 	}
 	cout << max_profit;
 	return 0;
